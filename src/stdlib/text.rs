997833863b1f263490c47c9fa02c2ea6c2ed04e6
Gianni Tedesco <gianni@scaramanga.co.uk>
2023-05-03 14:33:22 +0900

    Add text::len() and text::concat()

--- src/stdlib/text.rs
+++ src/stdlib/text.rs
@@ -1,12 +1,26 @@
 use phf::{phf_map, phf_ordered_map};
 
-use crate::val::{ValType, ValDef};
+use crate::val::{Val, ValType, ValDef};
+use crate::str::Buf;
 use crate::libapi::FuncDef;
 use crate::sym::Symbol;
 use crate::func_def;
 
-const TEXT_CRLFLINES: FuncDef = func_def!(
-    "crlflines";
+const CONCAT: FuncDef = func_def!(
+    "text::concat";
+    ValType::Str;
+
+    =>
+    =>
+    ValType::Str;
+
+    |mut args| {
+        Ok(args.join_extra(b""))
+    }
+);
+
+const CRLFLINES: FuncDef = func_def!(
+    "text::crlflines";
     ValType::Str;
 
     =>
@@ -18,7 +32,23 @@ const TEXT_CRLFLINES: FuncDef = func_def!(
     }
 );
 
+const LEN: FuncDef = func_def!(
+    "text::len";
+    ValType::U64;
+
+    =>
+    =>
+    ValType::Str;
+
+    |mut args| {
+        let buf: Buf = args.join_extra(b"").into();
+        Ok(Val::U64(buf.len() as u64))
+    }
+);
+
 pub const MODULE: phf::Map<&'static str, Symbol> = phf_map! {
-    "crlflines" => Symbol::Func(&TEXT_CRLFLINES),
+    "concat" => Symbol::Func(&CONCAT),
+    "crlflines" => Symbol::Func(&CRLFLINES),
+    "len" => Symbol::Func(&LEN),
     "CRLF" => Symbol::Val(ValDef::Str(b"\r\n")),
 };
