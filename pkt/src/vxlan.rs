1e79b9754d45ed9afb9184e109928658dcc721d7
Gianni Tedesco <gianni@scaramanga.co.uk>
2023-05-15 09:54:17 +0900

    vxlan: Update header structure

--- pkt/src/vxlan.rs
+++ pkt/src/vxlan.rs
@@ -1,9 +1,14 @@
 use super::Serialize;
 
+pub mod flags {
+    pub const I: u8 = 8;
+}
+
 #[repr(C, packed(1))]
 #[derive(Debug, Copy, Clone, Default)]
 pub struct vxlan_hdr {
-    pub flags: u32,
+    pub flags: u8,
+    pub reserved: [u8; 3],
     pub vni: u32,
 }
 
@@ -14,6 +19,7 @@ impl vxlan_hdr {
     pub const fn new() -> Self {
         Self {
             flags: 0,
+            reserved: [0; 3],
             vni: 0,
         }
     }
@@ -24,9 +30,19 @@ impl vxlan_hdr {
     }
 
     #[must_use]
-    pub const fn vni(mut self, vni: u32) -> Self {
-        self.flags |= 1u32 << 27;
-        self.vni = (vni << 8).to_be();
+    pub const fn i(mut self, flag: bool) -> Self {
+        if flag {
+            self.flags |= flags::I;
+        } else {
+            self.flags &= !flags::I;
+        }
         self
+    }
+
+    /// Use x.vni(123).i(false) to get a VNI without setting I flag
+    #[must_use]
+    pub const fn vni(mut self, vni: u32) -> Self {
+        self.vni = (vni << 8).to_be();
+        self.i(true)
     }
 }
