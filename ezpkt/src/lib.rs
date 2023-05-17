5242e6c36ceccc43481de0e8214516ac59f39269
Gianni Tedesco <gianni@scaramanga.co.uk>
2023-05-17 14:09:13 +0900

    Add support for ERSPAN II

--- ezpkt/src/lib.rs
+++ ezpkt/src/lib.rs
@@ -5,6 +5,7 @@ mod icmp4;
 mod vxlan;
 mod dhcp;
 mod gre;
+mod erspan2;
 
 pub use ip4::{IpDgram, IpFrag};
 pub use tcp4::{TcpSeg, TcpFlow};
@@ -13,3 +14,4 @@ pub use vxlan::{VxlanDgram, VxlanFlow};
 pub use icmp4::{IcmpDgram, IcmpFlow};
 pub use dhcp::Dhcp;
 pub use gre::{GreFlow, GreFrame};
+pub use erspan2::{Erspan2Flow, Erspan2Frame};
