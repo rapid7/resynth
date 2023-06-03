574e84b0b93e510b278af55b52007dbf74f792e2
Gianni Tedesco <gianni@scaramanga.co.uk>
2023-06-03 10:18:38 +0900

    Add TCP client_ack() and server_ack() methods

--- src/stdlib/ipv4/tcp.rs
+++ src/stdlib/ipv4/tcp.rs
@@ -23,116 +23,6 @@ const TCP_OPEN: FuncDef = func_def!(
     }
 );
 
-const TCP_CL_CLOSE: FuncDef = func_def!(
-    "ipv4::tcp::flow.client_close";
-    ValType::PktGen;
-
-    =>
-    =>
-    ValType::Void;
-
-    |mut args| {
-        let obj = args.take_this();
-        let mut r = obj.borrow_mut();
-        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
-        Ok(this.client_close().into())
-    }
-);
-
-const TCP_SV_CLOSE: FuncDef = func_def!(
-    "ipv4::tcp::flow.server_close";
-    ValType::PktGen;
-
-    =>
-    =>
-    ValType::Void;
-
-    |mut args| {
-        let obj = args.take_this();
-        let mut r = obj.borrow_mut();
-        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
-        Ok(this.server_close().into())
-    }
-);
-
-const TCP_CL_HOLE: FuncDef = func_def!(
-    "ipv4::tcp::flow.client_hole";
-    ValType::Void;
-
-    "bytes" => ValType::U32,
-    =>
-    =>
-    ValType::Void;
-
-    |mut args| {
-        let obj = args.take_this();
-        let mut r = obj.borrow_mut();
-        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
-        let bytes: u32 = args.next().into();
-
-        this.client_hole(bytes);
-        Ok(Val::Nil)
-    }
-);
-
-const TCP_SV_HOLE: FuncDef = func_def!(
-    "ipv4::tcp::flow.server_hole";
-    ValType::Void;
-
-    "bytes" => ValType::U32,
-    =>
-    =>
-    ValType::Void;
-
-    |mut args| {
-        let obj = args.take_this();
-        let mut r = obj.borrow_mut();
-        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
-        let bytes: u32 = args.next().into();
-
-        this.server_hole(bytes);
-        Ok(Val::Nil)
-    }
-);
-
-const TCP_CL_HDR: FuncDef = func_def!(
-    "ipv4::tcp::flow.client_hdr";
-    ValType::Str;
-
-    =>
-    "bytes" => ValDef::U32(0),
-    =>
-    ValType::Void;
-
-    |mut args| {
-        let obj = args.take_this();
-        let mut r = obj.borrow_mut();
-        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
-        let bytes: u32 = args.next().into();
-
-        Ok(Val::Str(Buf::from(this.client_hdr(bytes))))
-    }
-);
-
-const TCP_SV_HDR: FuncDef = func_def!(
-    "ipv4::tcp::flow.server_hdr";
-    ValType::Str;
-
-    =>
-    "bytes" => ValDef::U32(0),
-    =>
-    ValType::Void;
-
-    |mut args| {
-        let obj = args.take_this();
-        let mut r = obj.borrow_mut();
-        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
-        let bytes: u32 = args.next().into();
-
-        Ok(Val::Str(Buf::from(this.server_hdr(bytes))))
-    }
-);
-
 const TCP_CL_MSG: FuncDef = func_def!(
     "ipv4::tcp::flow.client_message";
     ValType::PktGen;
@@ -195,18 +85,180 @@ const TCP_SV_MSG: FuncDef = func_def!(
     }
 );
 
+const TCP_CL_HDR: FuncDef = func_def!(
+    "ipv4::tcp::flow.client_hdr";
+    ValType::Str;
+
+    =>
+    "bytes" => ValDef::U32(0),
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        let bytes: u32 = args.next().into();
+
+        Ok(Val::Str(Buf::from(this.client_hdr(bytes))))
+    }
+);
+
+const TCP_SV_HDR: FuncDef = func_def!(
+    "ipv4::tcp::flow.server_hdr";
+    ValType::Str;
+
+    =>
+    "bytes" => ValDef::U32(0),
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        let bytes: u32 = args.next().into();
+
+        Ok(Val::Str(Buf::from(this.server_hdr(bytes))))
+    }
+);
+
+const TCP_CL_ACK: FuncDef = func_def!(
+    "ipv4::tcp::flow.client_ack";
+    ValType::Pkt;
+
+    =>
+    "seq" => ValDef::Type(ValType::U32),
+    "ack" => ValDef::Type(ValType::U32),
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        let seq: Option<u32> = args.next().into();
+        let ack: Option<u32> = args.next().into();
+
+        let saved = this.push_state(seq, ack);
+        let pkt = this.client_ack();
+        this.pop_state(saved);
+
+        Ok(pkt.into())
+    }
+);
+
+const TCP_SV_ACK: FuncDef = func_def!(
+    "ipv4::tcp::flow.server_ack";
+    ValType::Pkt;
+
+    =>
+    "seq" => ValDef::Type(ValType::U32),
+    "ack" => ValDef::Type(ValType::U32),
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        let seq: Option<u32> = args.next().into();
+        let ack: Option<u32> = args.next().into();
+
+        let saved = this.push_state(seq, ack);
+        let pkt = this.server_ack();
+        this.pop_state(saved);
+
+        Ok(pkt.into())
+    }
+);
+
+const TCP_CL_HOLE: FuncDef = func_def!(
+    "ipv4::tcp::flow.client_hole";
+    ValType::Void;
+
+    "bytes" => ValType::U32,
+    =>
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        let bytes: u32 = args.next().into();
+
+        this.client_hole(bytes);
+        Ok(Val::Nil)
+    }
+);
+
+const TCP_SV_HOLE: FuncDef = func_def!(
+    "ipv4::tcp::flow.server_hole";
+    ValType::Void;
+
+    "bytes" => ValType::U32,
+    =>
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        let bytes: u32 = args.next().into();
+
+        this.server_hole(bytes);
+        Ok(Val::Nil)
+    }
+);
+
+const TCP_CL_CLOSE: FuncDef = func_def!(
+    "ipv4::tcp::flow.client_close";
+    ValType::PktGen;
+
+    =>
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        Ok(this.client_close().into())
+    }
+);
+
+const TCP_SV_CLOSE: FuncDef = func_def!(
+    "ipv4::tcp::flow.server_close";
+    ValType::PktGen;
+
+    =>
+    =>
+    ValType::Void;
+
+    |mut args| {
+        let obj = args.take_this();
+        let mut r = obj.borrow_mut();
+        let this: &mut TcpFlow = r.as_mut_any().downcast_mut().unwrap();
+        Ok(this.server_close().into())
+    }
+);
+
 impl Class for TcpFlow {
     fn symbols(&self) -> phf::Map<&'static str, Symbol> {
         phf_map! {
             "open" => Symbol::Func(&TCP_OPEN),
-            "client_close" => Symbol::Func(&TCP_CL_CLOSE),
-            "server_close" => Symbol::Func(&TCP_SV_CLOSE),
             "client_message" => Symbol::Func(&TCP_CL_MSG),
             "server_message" => Symbol::Func(&TCP_SV_MSG),
-            "client_hole" => Symbol::Func(&TCP_CL_HOLE),
-            "server_hole" => Symbol::Func(&TCP_SV_HOLE),
             "client_hdr" => Symbol::Func(&TCP_CL_HDR),
             "server_hdr" => Symbol::Func(&TCP_SV_HDR),
+            "client_ack" => Symbol::Func(&TCP_CL_ACK),
+            "server_ack" => Symbol::Func(&TCP_SV_ACK),
+            "client_hole" => Symbol::Func(&TCP_CL_HOLE),
+            "server_hole" => Symbol::Func(&TCP_SV_HOLE),
+            "client_close" => Symbol::Func(&TCP_CL_CLOSE),
+            "server_close" => Symbol::Func(&TCP_SV_CLOSE),
         }
     }
 
