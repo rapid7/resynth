77c184d1e1f771c505cc4860677fa71e680cbe9c
Gianni Tedesco <gianni@scaramanga.co.uk>
2023-06-04 14:05:43 +0900

    Make argument-parsing less ambiguous and more flexible
    Argument paring has become more relaxed:
     - Positional arguments may be named or nota
     - Optionals may be specified positionally for non-collect functions
    
    But there can be no ambiguity
     - For functions with collect args, optionals must be specified by name
     - No positional (or unnamed optional) can come appear after the first
       named argument
    
    And one thing has gotten stricter:
     - No argument may be multiply specified
    
    Add new test cases for this.

--- src/libapi.rs
+++ src/libapi.rs
@@ -6,6 +6,7 @@ use crate::args::{Args, ArgVec, ArgSpec};
 use crate::sym::Symbol;
 
 use std::fmt::Debug;
+use std::collections::HashMap;
 
 /// Argument declarator
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -40,34 +41,39 @@ pub trait Class {
     fn class_name(&self) -> &'static str;
 }
 
-struct NamedArg {
-    name: String,
-    val: Val,
-}
-
 struct ArgPrep {
-    anon: Vec<Val>,
-    named: Vec<NamedArg>,
+    positional: Vec<Val>,
+    named: HashMap<String, Val>,
     extra: Vec<Val>,
 }
 
+/// Invariant: No argument may be supplied with a value more than once
+/// Invariant: No argument can be ambiguous as to where it belongs
+///
+/// Rules:
+///  P-FIRST Positonal args must be supplied first
+///  P-NAME-OPTIONAL Positionals may be named or not
+///  ANON-FIRST If one positional is named, all subsequent positionals + optionals must be named
+///  COLLECT-NAME-OPTS if func has collect args, optionals MUST be named
+///  NOCOLLECT-ANON-OPTS if func doesn't have collect args, optionals MAY be anonymous
+///  COLLECT-AFTER-NAMED collect args must come after the last named arg
 impl FuncDef {
     pub fn is_collect(&self) -> bool {
         self.collect_type != ValType::Void
     }
 
-    fn split_args(&self,
-                      args: Vec<ArgSpec>,
-                      ) -> Result<ArgPrep, Error> {
+    fn arg_index(&self, name: &str) -> Option<usize> {
+        self.args.get_index(name)
+    }
+
+    fn split_args(&self, args: Vec<ArgSpec>) -> Result<ArgPrep, Error> {
         enum State {
             Anon,
             Named,
-            Extra,
-            Unexpected,
+            CollectOnly,
         }
-
-        let mut anon: Vec<Val> = Vec::new();
-        let mut named: Vec<NamedArg> = Vec::new();
+        let mut positional: Vec<Val> = Vec::new();
+        let mut named: HashMap<String, Val> = HashMap::new();
         let mut extra: Vec<Val> = Vec::new();
         let mut state = State::Anon;
 
@@ -78,132 +84,158 @@ impl FuncDef {
                         if !arg.is_anon() {
                             state = State::Named;
                             continue;
-                        } else if anon.len() >= self.min_args {
-                            state = State::Extra;
+                        } else if self.is_collect() && positional.len() >= self.min_args {
+                            // If we have collect args, then any optionals must be named
+                            state = State::CollectOnly;
                             continue;
+                        } else if positional.len() >= self.args.len() {
+                            if self.is_collect() {
+                                state = State::CollectOnly;
+                                continue;
+                            }
+                            println!("ERR: {}: Too many positional args: {} > {}",
+                                     self.name,
+                                     positional.len(),
+                                     self.args.len());
+                            return Err(TypeError);
                         } else {
-                            let ArgSpec { name: _, val } = arg;
-                            anon.push(val);
+                            positional.push(arg.val);
                             break;
                         }
                     },
                     State::Named => {
-                        if let ArgSpec { name: Some(name), val } = arg {
-                            named.push(NamedArg { name, val });
-                            break;
-                        } else {
-                            state = State::Extra;
+                        if arg.is_anon() {
+                            state = State::CollectOnly;
                             continue;
                         }
-                    },
-                    State::Extra => {
-                        if !self.is_collect() {
-                            println!("ERR: {} Unexpected extra arguments", self.name);
+
+                        let name = arg.name.unwrap();
+
+                        match self.arg_index(&name) {
+                            None => {
+                                println!("ERR: {}: No such argument: \"{}\"", self.name, &name);
+                                return Err(TypeError);
+                            }
+
+                            // Rule: Positionals must come first
+                            // Rule: First collect arg must be supplied after the last named arg
+                            // Invariant: No argument may be supplied with a value more than once
+                            //
+                            // Therefore:
+                            // the index in self.args of any named arg may not be >= the number of
+                            // positional args that have already been supplied before the first
+                            // named arg.
+                            //
+                            // Proof:
+                            // If n anon args have been supplied before the first named arg, then
+                            // the first n arguments in self.args have been specified. If any of
+                            // these are then subsequently named, an argument has had a value
+                            // supplied twice and the invariant is broken.
+                            Some(index) => {
+                                if index < positional.len() {
+                                    println!("ERR: {}: Positional arg \"{}\" multiply specified",
+                                             self.name, &name);
+                                    return Err(TypeError);
+                                }
+                            },
+                        }
+
+                        // Invariant: No argument may be supplied with a value more than once
+                        if named.contains_key(&name) {
+                            println!("ERR: {}: Argument \"{}\" multiply specified",
+                                     self.name, &name);
                             return Err(TypeError);
-                        }else if !arg.is_anon() {
-                            state = State::Unexpected;
-                            continue;
-                        } else {
-                            let ArgSpec { name: _, val } = arg;
-                            extra.push(val);
-                            break;
                         }
+
+                        named.insert(name, arg.val);
+                        break;
                     },
-                    State::Unexpected => {
-                        println!("ERR: {}: Unexpected named arguments \"{}\"",
-                                 self.name, arg.name.unwrap());
-                        return Err(TypeError);
-                    }
+                    State::CollectOnly => {
+                        if !self.is_collect() {
+                            println!("ERR: {}: Unexpected collect-arguments", self.name);
+                            return Err(TypeError);
+                        }
+                        if arg.is_named() {
+                            println!("ERR: {}: Unexpected named argument: \"{}\"",
+                                     self.name, arg.name.unwrap());
+                            return Err(TypeError);
+                        }
+                        extra.push(arg.val);
+                        break;
+                    },
                 }
             }
         }
 
-        anon.shrink_to_fit();
+        positional.shrink_to_fit();
         named.shrink_to_fit();
         extra.shrink_to_fit();
 
-        Ok(ArgPrep {anon, named, extra})
+        Ok(ArgPrep {positional, named, extra})
     }
 
     pub fn argvec(&self, this: Option<ObjRef>, args: Vec<ArgSpec>) -> Result<ArgVec, Error> {
-        let ArgPrep {anon, named, extra} = self.split_args(args)?;
-        let nr_anon = anon.len();
+        let ArgPrep {positional, mut named, extra} = self.split_args(args)?;
+        let nr_positional = positional.len();
         let nr_named = named.len();
-        let nr_specified = nr_anon + nr_named;
+        let nr_specified = nr_positional + nr_named;
 
         // Now do some basic sanity checks to stup us shooting ourselves in the foot later
         if nr_specified < self.min_args {
-            println!("ERR: Not enough specified args: {} ({} + {}) < {}",
+            println!("ERR: {}: Not enough specified args: {} ({} + {}) < {}",
+                     self.name,
                      nr_specified,
-                     nr_anon,
+                     nr_positional,
                      nr_named,
                      self.min_args);
             return Err(TypeError);
         }
 
-        if nr_anon > self.args.len() {
-            println!("ERR: Too many positional args: {} > {}",
-                     nr_anon,
-                     self.args.len());
-            return Err(TypeError);
-        }
-
-        if self.is_collect() && extra.iter().any(|x| !self.collect_type.compatible_with(x)) {
-            println!("ERR: Collect argument of wrong type");
-            return Err(TypeError);
-        }
-
-        // Now we actually start building the args struct
         let mut args: Vec<Val> = Vec::with_capacity(self.args.len());
 
         // 1. push anon vals to start with
-        for a in anon {
+        for a in positional {
             args.push(a);
         }
 
-        // 2. Fill in up to min_args with voids
-        for _ in nr_anon..self.min_args {
-            args.push(Val::Nil);
-        }
+        // either all positional and optional args are supplied and then everything else is in
+        // extra OR some positional/optional have been named, in which case all the collect args
+        // are in extra assert!(args.len() <= self.args.len());
 
-        // 3. Fill in defaults for named args
-        for dfl in self.args.values().skip(self.min_args) {
-            if let ArgDecl::Named(val) = dfl {
-                args.push((*val).into());
+        // 2. Take named positionals and optionals
+        for (name, spec) in self.args.entries().skip(nr_positional) {
+            if let Some(val) = named.remove(*name) {
+                // positional or optional specified by name, push it
+                args.push(val);
+            } else if let ArgDecl::Named(dfl) = spec {
+                // not specified, but we're optional, so take the default
+                args.push((*dfl).into());
             } else {
-                unreachable!();
-            }
-        }
-
-        // 4. Go over named arguments using map.get_index() to overwrite vals
-        for spec in named {
-            let NamedArg {name, val} = spec;
-
-            if let Some(i) = self.args.get_index(&name) {
-                args[i] = val;
-            } else {
-                println!("ERR: Unknown arg: {}", &name);
+                // not specified, and we're mandatory, barf
+                println!("ERR: {}: Positional argument \"{}\" not specified",
+                         self.name, name);
                 return Err(TypeError);
             }
         }
 
-        // 5. Check all mandatory args have been filled in
-        for val in &args[..self.min_args] {
-            if val.is_nil() {
-                println!("ERR: Mandatory argument not passed");
-                return Err(TypeError);
-            }
-        }
+        assert!(named.is_empty());
 
-        // 6. Final type-check of all positional args
+        // 3. Final type-check of all positional args
         for ((name, spec), arg) in self.args.entries().zip(args.iter()) {
             if !match spec {
                 ArgDecl::Positional(typ) => { typ.compatible_with(arg) }
                 ArgDecl::Named(dfl) => { dfl.arg_compatible(arg) },
             } {
-                println!("ERR: Argument type-check failed for {:?}", name);
+                println!("ERR: {}: Argument type-check failed for {:?}",
+                         self.name, name);
                 return Err(TypeError);
             }
+        }
+
+        // 4. Type-check the collect-args
+        if extra.iter().any(|x| !self.collect_type.compatible_with(x)) {
+            println!("ERR: {}: Collect argument of wrong type", self.name);
+            return Err(TypeError);
         }
 
         Ok(ArgVec::new(this, args, extra))
